ed5aef4781e3815902f91f8a1544e53d4380dec1
mastuyink <aziest99@gmail.com>
2018-03-18 22:43:41 +0700

    perubahan file header

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -63,7 +63,7 @@ use yii\helpers\Html;
                 <!-- User Account: style can be found in dropdown.less -->
                 <li>
                     <a href="#" data-toggle="control-sidebar"></a>
-                </li
+                </li>
             <?php endif; ?>
             </ul>
         </div>
